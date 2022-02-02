fac9b5c5d1f970b323f6ef052da4d250570dfbac
Tengku Zulfadli <62935899+tengkuzulfadli@users.noreply.github.com>
2022-02-02 11:54:46 +1100

    News App - React Native Expo

---
+++ config/Config.js
@@ -0,0 +1,3 @@
+export const API_KEY = `ab5401b5317946e8b1295054c7f264d9`;
+export const endpoint = `https://newsapi.org/v2/top-headlines`;
+export const country = 'au';
---
+++ screens/Sports.js
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, LogBox } from 'react-native';
+import { NativeBaseProvider, FlatList, ScrollView, Divider, Image, Spinner, SafeAreaView } from 'native-base';
+import { services } from '../services/Services';
+import moment from 'moment';
+
+export default function SportsScreen() {
+    const [newsData, setNewsData] = useState([])
+    useEffect(() => {
+        services('sports')
+            .then(data => {
+                setNewsData(data)
+            })
+            .catch(error => {
+                alert(error)
+            })
+    }, [])
+    return (
+        <NativeBaseProvider>
+            
+                {newsData.length > 1 ? (
+                    <FlatList
+                        data={newsData}
+                        keyExtractor={(item, index) => index.toString()}
+                        renderItem={({ item }) => (
+                            <View>
+                                <View style={styles.newsContainer}>
+                                    <Image
+                                        width={550}
+                                        height={250}
+                                        resizeMode={"cover"}
+                                        source={{
+                                            uri: item.urlToImage,
+                                        }}
+                                        alt="Alternate Text"
+                                    />
+                                    <Text style={styles.title}>
+                                        {item.title}
+                                    </Text>
+                                    <Text style={styles.date}>
+                                        {moment(item.publishedAt).format('LLL')}
+                                    </Text>
+                                    <Text style={styles.newsDescription}>
+                                        {item.description}
+                                    </Text>
+                                </View>
+                                <Divider my={2} bg="#e0e0e0" />
+                            </View>
+                        )}
+                    />
+                ) : (
+                    <View style={styles.spinner}>
+                        <Spinner color="danger.400" />
+                    </View>
+                )}
+        </NativeBaseProvider>
+    )
+}
+
+const styles = StyleSheet.create({
+    newsContainer: {
+        padding: 10
+    },
+    title: {
+        fontSize: 18,
+        marginTop: 10,
+        fontWeight: "600"
+    },
+    newsDescription: {
+        fontSize: 16,
+        marginTop: 10
+    },
+    date: {
+        fontSize: 14
+    },
+    spinner: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 400
+    }
+});
